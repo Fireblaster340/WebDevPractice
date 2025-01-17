32de6305e1945482423664b8d213bc41a124857b
Fireblaster340 <reyhaans2008@gmail.com>
2025-01-17 22:55:00 +0000

    Commit1

---
+++ ChangeTxt.js
@@ -0,0 +1,28 @@
+let i;
+function Change() {
+    let j = 3, a = 4;
+    document.getElementById("maintext").innerHTML = "Whoa!"+j
+    document.getElementById("buttontext").innerHTML = "You pressed me!"+a
+    document.getElementById("top").innerHTML = "Click the button at the botom to change the ttext below!"
+    newText = "<u>Updated Wow!</u>"
+
+}
+let k = false;
+let b = new Date();
+b.setTime(3000000000)
+document.cookie = "clicks=99; expires="+b.toUTCString()
+let newText= "<b>Well... </b>"
+function SwitchVar(){
+    /*if (confirm("Are you sure?") == false) {
+        return 0;
+    }
+        */
+    if (k==true ) {
+        document.getElementById("buttonsubmit").innerHTML = newText
+        k=false
+    } else {
+        document.getElementById("buttonsubmit").innerHTML = "<em>Whooa</em>"
+        k=true
+    }
+    //prompt("Hi, enter absolutely anything, I really don't care")
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WebDevPractice
+Very Cool! I'm learning JavaScript and HTML to make web games! My goal: Make a chrome extension game!
