4b6a83c8a062e8da78e273c2472382addb25b8c9
Fireblaster340 <reyhaans2008@gmail.com>
2025-01-18 22:17:53 +0000

    Cookie Increment Try #1

--- ChangeTxt.js
+++ ChangeTxt.js
@@ -1,7 +1,6 @@
-let i;
+
 function Change() {
-    let j = 3, a = 4;
-    document.getElementById("maintext").innerHTML = "Whoa!"+j
+    document.getElementById("maintext").innerHTML = "Whoa!"
     document.getElementById("buttontext").innerHTML = "You pressed me!"
     document.getElementById("top").innerHTML = "Click the button at the botom to change the ttext below!"
     newText = "<u>Updated Wow!</u>"
@@ -10,10 +9,16 @@ function Change() {
 let k = false;
 let b = new Date();
 b.setTime(b.getTime()+(30*24*60*60*1000))
-document.cookie = "clicks=99; expires="+b.toUTCString()+"; path=/";
-alert(document.cookie)
+function IncrementCookie(){
+    document.cookie = "clicks="+(document.cookie+1)
+
+
+if (document.cookie.includes("clicks=")==false){
+    document.cookie = "clicks=0; expires="+b.toUTCString()+"; path=/";
+}
 let newText= "<b>Well... </b>"
 function SwitchVar(){
+    IncrementCookie()
     alert(document.cookie)
     /*if (confirm("Are you sure?") == false) {
         return 0;
